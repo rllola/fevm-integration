ec974a080991a602caa4d4b0c09b415dbbc3a85d
rllola <rllola80@gmail.com>
2023-01-05 10:39:30 +0100

    poc; deploying singletons with 'constructor'

--- testing/src/main.rs
+++ testing/src/main.rs
@@ -5,13 +5,21 @@ use fvm_ipld_encoding::{strict_bytes, tuple::*};
 use fvm_shared::state::StateTreeVersion;
 use fvm_shared::version::NetworkVersion;
 use fvm_ipld_blockstore::MemoryBlockstore;
-use fvm_shared::address::Address;
+use anyhow::{Context, Result};
+use fvm_ipld_encoding::CborStore;
+use cid::Cid;
+use fvm_ipld_blockstore::Blockstore;
+use fvm_shared::ActorID;
+use fvm::state_tree::{ActorState, StateTree};
+use multihash::Code;
 use std::env;
+use std::str::FromStr;
 use fvm_shared::message::Message;
 use fvm::executor::{ApplyKind, Executor};
 use fil_actor_eam::Return;
 use fvm_ipld_encoding::RawBytes;
 use fil_actors_runtime::{EAM_ACTOR_ADDR};
+use fvm_shared::address::Address;
 
 const WASM_COMPILED_PATH: &str =
 "../build/SimpleCoin.bin";
@@ -37,24 +45,49 @@ fn main() {
 
     let sender: [Account; 1] = tester.create_accounts().unwrap();
 
-    let wasm_path = env::current_dir()
-    .unwrap()
-    .join(WASM_COMPILED_PATH)
-    .canonicalize()
-    .unwrap();
-    let evm_bin = std::fs::read(wasm_path).expect("Unable to read file");
+    // INIT STORAGE POWER SINGLETON
+    let state_tree = tester.state_tree.as_mut().unwrap();
+    let storagemarket_code_cid = Cid::from_str("bafk2bzacedreyxpkabexd4lrmcjzenfkjyfrbzns2o3p7cokfm6b7iendal2o").unwrap();
 
-    let constructor_params = Create2Params {
-        initcode: evm_bin,
-        salt: [0; 32],
-    };
+    set_storagepower_actor(state_tree, storagemarket_code_cid);
 
     // Instantiate machine
     tester.instantiate_machine(DummyExterns).unwrap();
 
     let executor = tester.executor.as_mut().unwrap();
 
+    // Try to call "constructor"
+    println!("Try to call constructor on storage power actor");
+
+    let message = Message {
+        from: Address::new_id(0),
+        to: Address::new_id(4),
+        gas_limit: 1000000000,
+        method_num: 2,
+        ..Message::default()
+    };
+
+    let res = executor
+        .execute_message(message, ApplyKind::Explicit, 100)
+        .unwrap();
+
+    dbg!(&res);
+
+    assert_eq!(res.msg_receipt.exit_code.value(), 0);
+
     println!("Calling init actor (EVM)");
+
+    let wasm_path = env::current_dir()
+        .unwrap()
+        .join(WASM_COMPILED_PATH)
+        .canonicalize()
+        .unwrap();
+    let evm_bin = std::fs::read(wasm_path).expect("Unable to read file");
+
+    let constructor_params = Create2Params {
+        initcode: evm_bin,
+        salt: [0; 32],
+    };
 
     let message = Message {
         from: sender[0].1,
@@ -74,4 +107,30 @@ fn main() {
     assert_eq!(res.msg_receipt.exit_code.value(), 0);
 
     let exec_return : Return = RawBytes::deserialize(&res.msg_receipt.return_data).unwrap();
+}
+
+
+pub fn set_storagepower_actor(
+    state_tree: &mut StateTree<impl Blockstore>,
+    storagepower_code_cid: Cid,
+) -> Result<()> {
+    const STORAGE_POWER_ACTOR: ActorID = 4;
+
+    let storagepower_state_cid = state_tree
+        .store()
+        .put_cbor(&[(); 0], Code::Blake2b256)
+        .context("storagepower actor".to_owned())?;
+
+    let storagepower_actor_state = ActorState {
+        code: storagepower_code_cid,
+        state: storagepower_state_cid,
+        sequence: 0,
+        balance: Default::default(),
+        delegated_address: None,
+    };
+
+    state_tree
+        .set_actor(STORAGE_POWER_ACTOR, storagepower_actor_state)
+        .map_err(anyhow::Error::from)
+        .context("storagepower actor".to_owned())
 }
